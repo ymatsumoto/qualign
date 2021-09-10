d201263f1549345aac41642365998f51630b3d71
Yuki Matsumoto <ymts43@gmail.com>
2021-09-10 14:25:29 +0900

    Updated for using new version of pyqubo

--- src/qualign.py
+++ src/qualign.py
@@ -5,10 +5,14 @@ import functools
 import operator
 import json
 import copy
-from pyqubo import Array, Sum, Constraint, Placeholder, utils
+import neal
+import sys
+import numpy as np
+from pyqubo import Array, Constraint, Placeholder, utils, Spin
 from pprint import pprint
 from solveda import solve_DA, solve_DA2, solve_DAPT, solve_DA2PT, solve_DAMixed, solve_DA2Mixed, to_sol, result
 from blosum62 import Blosum62
+from argparse import ArgumentParser
 
 def generateCI_src(index, crossed_index, i1, i2):
     src="("
@@ -56,7 +60,8 @@ def conv(index, dim):
     tmp = copy.deepcopy(index)
     for i in range(slen[dim]):
         tmp[dim] = i
-        exp += i2x(tmp)
+        #exp += i2x(tmp) # for BINARY
+        exp += (i2x(tmp) + 1) / 2
     return(exp)
 
 def score(index):
@@ -68,55 +73,117 @@ def score(index):
 def maxScore():
     return(N*(N-1)/2)
 
-blosum62 = Blosum62()
-def score_blosum62(index):
+m = Blosum62()
+def score_blosum62(index, matrix):
     nucl = []
     for i in range(N):
         nucl.append(s[i][index[i]])
-    return(sum(list(map(lambda a: blosum62.score(a[0],a[1]), itertools.combinations(nucl, 2)))))
+    return(sum(list(map(lambda a: matrix.score(a[0],a[1]), itertools.combinations(nucl, 2)))))
     #return(sum(list(map(lambda a: Blosum62.scoreblosum62[a[0]][a[1]], itertools.combinations(nucl, 2)))))
-
 
 def maxScore_blosum62():
     return(N*(N-1)/2)
 
-#s = ["ATGCATC", "ATCAC", "ATCACCCTAA"]
-max_gaps = 5
-s = ["ATGCATC", "ATCAC"]
+def parser():
+    argparser = ArgumentParser()
+    argparser.add_argument('-s1', help='AA Sequence 1', required=True)
+    argparser.add_argument('-s2', help='AA Sequence 2', required=True)
+    argparser.add_argument('-c0', help='Penalty for AA mismatch (default=4)', type=int, default=1)
+    argparser.add_argument('-c1', help='Penalty for redundant alignment (default=8)', type=int, default=8)
+    argparser.add_argument('-c2', help='Penalty for twisted alignment (default=8)', type=int, default=4)
+    #argparser.add_argument('-g', '--gap', help='Number of outer maximum gaps (default=0)', default=0)
+    argparser.add_argument('-q', '--quiet', help='Print only results', action='store_true', default=False)
+    args = argparser.parse_args()
+    return args
 
-diff, i = (len(s[0])-len(s[1]), 0) if len(s[0]) > len(s[1]) else (len(s[1])-len(s[0]), 1)
-if diff < max_gaps:
-    s[i] += '*'*(max_gaps - diff)
+if __name__ == '__main__':
+    args = parser()
+    #s = ["ATGCATC", "ATCAC"]
+    s = [args.s1, args.s2]
 
-a,b,c,d,e = Placeholder('a'), Placeholder('b'), Placeholder('c'), Placeholder('d'), Placeholder('e')
-fd = {'a':0, 'b':1, 'c':0, 'd':8, 'e':4}
+    #max_gaps = args.gap
+    #diff, i = (len(s[0])-len(s[1]), 0) if len(s[0]) > len(s[1]) else (len(s[1])-len(s[0]), 1)
+    #if diff < max_gaps:
+    #    s[i] += '*'*(max_gaps - diff)
 
-N = len(s)
-slen = list(map(lambda s: len(s), s))
-x = Array.create('x', shape=slen, vartype='BINARY')
+    c0,c1,c2 = Placeholder('C0'), Placeholder('C1'), Placeholder('C2')
+    fd = {'C0':args.c0, 'C1':args.c1, 'C2':args.c2}
 
-index_all = [list(i) for i in eval("itertools.product("+",".join(map(lambda s: "range("+str(s)+")", slen))+")")]
-exp = b*sum((-score_blosum62(i))*i2x(i) for i in index_all)
-for dim in range(N):
-    tmp = copy.deepcopy(slen)
-    tmp[dim] = 1
-    index = [list(i) for i in eval("itertools.product("+",".join(map(lambda s: "range("+str(s)+")", tmp))+")")]
-    exp_tmp = 0
-    for i in index:
-        exp_tmp += d*(conv(i, dim) - 1.0)**2
-    exp += Constraint(exp_tmp, label="one_dim")
-for index in index_all:
-    ci = []
-    for i1, i2 in itertools.combinations(range(N), 2):
-        ci += generateCrossIndex_i(index, i1, i2)
-        exp += Constraint(e*(i2x(index)*sum(i2x(i) for i in ci)), label="cross")
+    N = len(s)
+    slen = list(map(lambda s: len(s), s))
+    print(slen)
+    x = Array.create('x', shape=slen, vartype='SPIN')
 
-model = exp.compile()
-q, offset = model.to_qubo(feed_dict=fd)
+    index_all = [list(i) for i in eval("itertools.product("+",".join(map(lambda s: "range("+str(s)+")", slen))+")")]
+    exp = c0*sum((-score_blosum62(i, m))*(i2x(i)+1)/2 for i in index_all)
+    print(exp)
+    #exp = c0*sum((-score_blosum62(i, m))*i2x(i) for i in index_all) for BINARY
+    for dim in range(N):
+        tmp = copy.deepcopy(slen)
+        tmp[dim] = 1
+        index = [list(i) for i in eval("itertools.product("+",".join(map(lambda s: "range("+str(s)+")", tmp))+")")]
+        exp_tmp = 0
+        for i in index:
+            exp_tmp += c1*(conv(i, dim) - 1.0)**2
+        exp += Constraint(exp_tmp, label="one_dim")
+    for index in index_all:
+        ci = []
+        for i1, i2 in itertools.combinations(range(N), 2):
+            ci += generateCrossIndex_i(index, i1, i2)
+            #exp += Constraint(c2*(i2x(index)*sum(i2x(i) for i in ci)), label="cross") # for BINARY
+            exp += Constraint(c2*((i2x(index)+1)/2*sum((i2x(i)+1)/2 for i in ci)), label="cross")
 
-sol = utils.solve_qubo(q, sweeps=1000)
+    model = exp.compile()
+    bqm = model.to_bqm(feed_dict=fd)
+    sa = neal.SimulatedAnnealingSampler()
+    sampleset = sa.sample(bqm, num_reads=10)
+    samples = model.decode_sampleset(sampleset, fd)
+    sol = min(samples, key=lambda s: s.energy)
+    xx = list(dict(sorted(sol.sample.items())).values())
+    xx = np.array(xx).reshape(*map(len, s))
 
-##pprint({(k,v) for k,v in sol.items() if v == 1})
-pprint(sorted([k for k,v in sol.items() if v == 1]))
-decoded_solution, broken, energy =\
-  model.decode_solution(sol, feed_dict=fd, vartype='BINARY')
+    print(xx)
+    success = True
+    sum1 = np.sum(xx, axis=0)
+    sum2 = np.sum(xx, axis=1)
+    i = j = 0
+    while i < slen[0] and j < slen[1]:
+        if xx[i][j] == 1:
+            i = i + 1
+            j = j + 1
+            continue
+        if xx[i][j] == 0:
+            if sum1[j] >= 2 or sum2[i] >= 2:
+                success = False
+                break
+            if sum1[j] == 0 and sum2[i] != 0:
+                j = j + 1
+                continue
+            if sum1[j] != 0 and sum2[i] == 0:
+                i = i + 1
+                continue
+            else:
+                success = False
+                break
+
+    if not success and not args.quiet:
+            print("Alignment failed.", file=sys.stderr)
+
+    if success:
+        res = bytearray()
+        for i in range(slen[0]):
+            if sum2[i] == 0:
+                s[1] = s[1][:i] + '-' + s[1][i:]
+                #s[1].insert(i, '-')
+        for j in range(slen[1]):
+            if sum1[j] == 0:
+                s[0] = s[0][:j] + '-' + s[0][j:]
+                #s[0].insert(j, '-')
+        score = 0
+        for i in range(slen[0]):
+            score -= m.blosum62[s[0][i]][s[1][i]]
+        if not args.quiet:
+            print("Alignment successed. Score:",+score, file=sys.stderr)
+        print(s[0])
+        print(s[1])
+
